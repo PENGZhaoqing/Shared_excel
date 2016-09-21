7b7c0304df7fa14aca5dce003f6edfd578ef45b9
peng <ppp8300885@gmail.com>
2016-09-21 19:21:39 +0800

    convert to where in postgresql

--- app/models/stock_db.rb
+++ app/models/stock_db.rb
@@ -9,10 +9,9 @@ class StockDb < ActiveRecord::Base
                 .where('stock_dbs.standard LIKE ?', "%#{params[:standard]}%")
                 .where('stock_dbs.kind LIKE ?', "%#{params[:kind]}%")
                 .where('stock_dbs.supplier LIKE ?', "%#{params[:supplier]}%")
-    @result.find_by(export_num: params[:export_num]) if !params[:export_num].blank?
+    @result=@result.where('stock_dbs.export_num = ?', params[:export_num]) if !params[:export_num].blank?
     return @result
   end
-
 
   def self.filter_by_type(type)
     where("supplier = :type", type: type)
