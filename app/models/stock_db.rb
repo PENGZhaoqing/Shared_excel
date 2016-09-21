808a08d11f2c9ef806c15722b573968356b2b0e1
peng <ppp8300885@gmail.com>
2016-09-21 19:07:59 +0800

    remove find_by in postgresql

--- app/models/stock_db.rb
+++ app/models/stock_db.rb
@@ -1,19 +1,16 @@
 class StockDb < ActiveRecord::Base
 
   def self.search(params)
-    if params[:export_num].blank?
-      @result=StockDb.where('stock_dbs.complete_time LIKE ?', "%#{params[:complete_time]}%")
-                  .where('stock_dbs.client_name LIKE ?', "%#{params[:client_name]}%")
-                  .where('stock_dbs.product_name LIKE ?', "%#{params[:product_name]}%")
-                  .where('stock_dbs.product_code LIKE ?', "%#{params[:product_code]}%")
-                  .where('stock_dbs.standard LIKE ?', "%#{params[:standard]}%")
-                  .where('stock_dbs.kind LIKE ?', "%#{params[:kind]}%")
-                  .where('stock_dbs.supplier LIKE ?', "%#{params[:supplier]}%")
-    else
-      @result.find_by(:export_num, params[:export_num])
-    end
 
-    return @result
+    @result=StockDb.where('stock_dbs.complete_time LIKE ?', "%#{params[:complete_time]}%")
+                .where('stock_dbs.client_name LIKE ?', "%#{params[:client_name]}%")
+                .where('stock_dbs.product_name LIKE ?', "%#{params[:product_name]}%")
+                .where('stock_dbs.product_code LIKE ?', "%#{params[:product_code]}%")
+                .where('stock_dbs.standard LIKE ?', "%#{params[:standard]}%")
+                .where('stock_dbs.kind LIKE ?', "%#{params[:kind]}%")
+                .where('stock_dbs.supplier LIKE ?', "%#{params[:supplier]}%")
+    @result.where('stock_dbs.export_num = ?', params[:export_num]) if params[:export_num].blank?
+
   end
 
 
