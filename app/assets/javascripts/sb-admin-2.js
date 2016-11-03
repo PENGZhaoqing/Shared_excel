6ff08f20694359317bb43d8eef8cf5105405db4a
peng <ppp8300885@gmail.com>
2016-11-03 12:16:25 +0800

    table commplete

--- app/assets/javascripts/sb-admin-2.js
+++ app/assets/javascripts/sb-admin-2.js
@@ -5,6 +5,7 @@
  */
 
 
+
 $(document).ready(function () {
     $("body").on("click", '.sort_paginate_ajax .pagination a', function (e) {
         e.preventDefault();
@@ -63,3 +64,4 @@ $(function() {
         }
     }
 });
+
