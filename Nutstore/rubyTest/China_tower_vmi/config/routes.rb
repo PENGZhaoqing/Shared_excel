29e1f12b50bd7974a79d09addbdbd276eb04b703
peng <ppp8300885@gmail.com>
2016-11-15 20:52:30 +0800

    delete app and db

---
+++ Nutstore/rubyTest/China_tower_vmi/config/routes.rb
@@ -0,0 +1,97 @@
+Rails.application.routes.draw do
+
+  root 'homes#index'
+
+  resources :supplier_excels, only: [:index, :destroy, :create] do
+    collection do
+      get :clean
+    end
+    member do
+      get :parse
+    end
+  end
+
+  resources :supplier_dbs, only: :index do
+    collection do
+      get :export
+    end
+  end
+
+  resources :repertory_excels, only: [:index, :destroy, :create] do
+    collection do
+      get :clean
+    end
+    member do
+      get :parse
+    end
+  end
+
+  resources :repertory_dbs, only: :index do
+    collection do
+      get :export
+    end
+  end
+
+  resources :project_excels, only: [:index, :destroy, :create] do
+    collection do
+      get :clean
+    end
+    member do
+      get :parse
+    end
+  end
+
+
+  resources :project_dbs, only: :index do
+    collection do
+      get :export
+    end
+  end
+
+  resources :stock_excels, only: [:index, :destroy, :create] do
+    collection do
+      get :clean
+    end
+    member do
+      get :parse
+    end
+  end
+
+  resources :stock_dbs, only: :index do
+    collection do
+      get :export
+    end
+  end
+
+  resources :mapping_excels, only: [:index, :destroy, :create] do
+    collection do
+      get :clean
+    end
+    member do
+      get :parse
+    end
+  end
+
+  resources :mapping_dbs, only: :index do
+    collection do
+      get :export
+    end
+  end
+
+  resources :users, except: [:new, :show] do
+    collection do
+      get :admin_new
+      post :admin_create
+    end
+    member do
+      patch :admin_update
+    end
+  end
+
+  post 'sessions/login' => 'sessions#create'
+  post 'sessions/visit' => 'sessions#visit'
+  delete 'sessions/logout' => 'sessions#destroy'
+  delete 'sessions/visit_out' => 'sessions#destroy_visit'
+
+
+end
