0465a85652064e5fe0e49cd45edce39e5ec72423
peng <ppp8300885@gmail.com>
2016-11-15 21:00:04 +0800

    delete app and db

--- db/migrate/20160722053651_create_repertory_excels.rb
+++
@@ -1,8 +0,0 @@
-class CreateRepertoryExcels < ActiveRecord::Migration
-  def change
-    create_table :repertory_excels do |t|
-      t.attachment :file
-      t.timestamps null: false
-    end
-  end
-end
